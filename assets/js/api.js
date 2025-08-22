6a0ac08b28166b9b3734b63da9d372ef9c60944c
maxadobr <maxado.mdx@gmail.com>
2025-08-22 18:13:43 -0300

    fix(profile.json): fixed experiences typo

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/maxadobr/portfolio/refs/heads/main/data/profile.json'
+
     const fetching = await fetch(url)
     return await fetching.json()
 }
