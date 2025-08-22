247409bfcccf64a356756ebed403eb8365d15c96
maxadobr <maxado.mdx@gmail.com>
2025-08-22 18:09:12 -0300

    feat: added experiences

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,25 @@ function updateHardSkills(profileData) {
 function updateLanguages(profileData) {
     const languages = document.querySelector('.languages');
 
-    languages.innerHTML = profileData.languages.map(language => `<li>${language.lang}${language.level}</li>`).join('');
+    languages.innerHTML = profileData.languages.map(language => `<li>${language.lang} (${language.level})</li>`).join('');
+}
+
+/*
+<li>
+    <h3 class="title">Front-end Developer / dio.me</h3>
+    <p class="period">Jan 2020 - Current</p>
+    <p>
+        Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est
+        reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure,
+        iste.
+    </p>
+</li>
+*/
+
+function updateExperiences(profileData) {
+    const experiences = document.querySelector('.experiences');
+
+    experiences.innerHTML = profileData.experiences.map(experience => `<li><h3 class="title">${experience.position} / ${experience.company}</h3><p class="period">${experience.period.start} ${experience.period.finish}</p></li>`).join('');
 }
 
 (async () => {
@@ -42,5 +60,6 @@ function updateLanguages(profileData) {
     updateHardSkills(profileData)
     updateLanguages(profileData)
 
+    updateExperiences(profileData)
     console.log(profileData)
 })()
