d71c07a0178294abe5641d094bf02d8240a743d4
maxadobr <maxado.mdx@gmail.com>
2025-08-22 14:21:59 -0300

    feat: added softskills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,26 @@
 function updateProfileInfo(profileData) {
-    const photo = document.getElementsByClassName('.profile_photo');
+    const photo = document.querySelector('.profile_photo');
     photo.src = profileData.photo
     photo.alt = profileData.name
 
-    const name = document.getElementsByClassName('.profile_name');
+    const name = document.querySelector('.profile_name');
     name.innerText = profileData.name
-    console.log(name)
 
-    const title = document.getElementsByClassName('.profile_title');
+    const title = document.querySelector('.profile_title');
     title.innerText = profileData.title
 
-    const location = document.getElementsByClassName('.profile_location');
-    location.innerText = profileData.location
+    const location = document.querySelector('.profile_location');
+    location.innerText = profileData.location.country
 
-    const email = document.getElementsByClassName('.profile_email');
+    const email = document.querySelector('.profile_email');
     email.innerText = profileData.email
+}
+
+function updateSoftSills(profileData) {
+    const softSkills = document.querySelector('.soft_skills');
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
+
 }
 
 (async () => {
