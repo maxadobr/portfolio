c3b0c6095cbf7dd1f44d97ac7cd496374b4b8b00
maxadobr <maxado.mdx@gmail.com>
2025-08-22 13:48:33 -0300

    fix: proprieties in profile.json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 function updateProfileInfo(profileData) {
-    const photo = document.getElementsByClassName('profile_photo');
+    const photo = document.getElementsByClassName('.profile_photo');
     photo.src = profileData.photo
     photo.alt = profileData.name
+
+    const name = document.getElementsByClassName('.profile_name');
+    name.innerText = profileData.name
+    console.log(name)
+
+    const title = document.getElementsByClassName('.profile_title');
+    title.innerText = profileData.title
+
+    const location = document.getElementsByClassName('.profile_location');
+    location.innerText = profileData.location
+
+    const email = document.getElementsByClassName('.profile_email');
+    email.innerText = profileData.email
 }
 
 (async () => {
